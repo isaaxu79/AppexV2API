e6e3cd3e8974d1d40143a0c68ad3433dcb093c89
Isaac A. Marin <VasMar_ia@hotmail.com>
2020-02-25 21:36:03 -0600

    sdssa

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 
 from django.contrib.auth import authenticate
 from django.views.decorators.csrf import csrf_exempt
+
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
@@ -22,11 +23,13 @@ def login(request):
         return Response({'error': 'Please provide both username and password'},
                         status=HTTP_400_BAD_REQUEST)
     user = authenticate(username=username, password=password)
+    sd = user.is_superuser
+    print(sd)
     if not user:
         return Response({'error': 'Invalid Credentials'},
                         status=HTTP_404_NOT_FOUND)
     token, _ = Token.objects.get_or_create(user=user)
-    return Response({'token': token.key},
+    return Response({'token': token.key,'super':sd},
                     status=HTTP_200_OK)
 
 @csrf_exempt
